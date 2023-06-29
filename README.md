5838053aadbac88be31fcf498926f9f0bcfc3328
igor <igor@kiselev.ru.net>
2023-06-29 16:35:22 +0700

    README.md update

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
+## Langluages
+
+- [Русский](https://github.com/NagibatorIgor/ccp/blob/main/ПРОЧИТАЙ.md)
+
 ## About the project
 
 Console Control Panel is a convenient, lightweight, secure, simple Linux control panel. Its main feature is communication with the user via the console, which significantly improves security compared to other control panels. This control panel was inspired by the msconfig utility.
+
+## Install
+
+
 
 ## Links
 
--- installcсp.sh
+++ installcсp.sh
@@ -1,1 +1,56 @@
 #!/bin/bash
+# Проверка дистрибутива Linux
+if [[ "$(lsb_release -si)" != "Debian" && "$(lsb_release -si)" != "Ubuntu" ]]; then
+    echo "So far, ConsoleCP supports only Debian and Ubuntu."
+    exit 1
+fi
+
+# Проверка пользователя root
+if [[ $EUID -ne 0 ]]; then
+    echo "You need to run install with root permissions"
+    exit 1
+fi
+
+# Создание директории /opt/ccp
+mkdir -p /opt/ccp
+
+wget -O /opt/ccp/ccp.zip https://github.com/NagibatorIgor/ccp/archive/main.zip
+unzip /opt/ccp/ccp.zip -d /opt/ccp
+mv /opt/ccp/ccp-main/* /opt/ccp
+rm -rf /opt/ccp/ccp-main
+rm /opt/ccp/ccp.zip
+rm /opt/ccp/installccp.sh
+
+# Установка прав доступа
+chmod -R 755 /opt/ccp
+
+# Добавление PATH для /opt/ccp/ccp.sh
+echo 'export PATH="/opt/ccp:$PATH"' >> /etc/profile
+
+# Обновление текущего окружения
+source /etc/profile
+
+# Запрос языка установки
+echo "Select langluage:"
+    echo "1. English"
+    echo "2. Русский"
+
+    tput cup $(tput lines) 0
+    read -p "Select: " lang
+
+    case $lang in
+        1)
+            lang_code="en"
+            ;;
+        2)
+            lang_code="ru"
+            ;;
+        *)
+            echo "Fail choise"
+            return
+            ;;
+    esac
+
+echo $lang_code > /opt/ccp/lang.config  # Запись выбранного языка в файл
+
+echo "The installation has been completed successfully. Use ccp to bring up the control panel."
