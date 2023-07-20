7765ffae06ff9eb020cea1ee4ff3eb598286586d
igor <igor@kiselev.ru.net>
2023-07-20 07:51:53 +0700

    lang settings fix

--- ccp.sh
+++ ccp.sh
@@ -275,7 +275,7 @@ change_language() {
             ;;
     esac
 
-    echo $lang_code > "lang.config"  # Запись выбранного языка в файл
+    echo $lang_code > "/opt/ccp/lang.config"  # Запись выбранного языка в файл
     echo "$SUCCESS"
     read -n 1 -s -r -p "$ANYKEY_CONTINUE"
 }
@@ -947,6 +947,14 @@ case $command in
 esac
 }
 
+center_text() {
+    local text="$1"
+    local width="$2"
+    local padding=$(( (width - ${#text}) / 2 ))
+    printf "%*s%s%*s
+" $padding "" "$text" $padding ""
+}
+
 # Основное меню
 while true; do
 
@@ -957,30 +965,56 @@ load_language_resources
   echo "$NON_ROOT" 
   exit 1
   fi
+    width=$(tput cols)
     clear
-    echo "   ___                      _        ___   ___ "
-    echo "  / __\___  _ __  ___  ___ | | ___  / __\ / _ |"
-    echo " / /  / _ \| '_ \/ __|/ _ \| |/ _ \/ /   / /_)/"
-    echo "/ /__| (_) | | | \__ \ (_) | |  __/ /___/ ___/ "
-    echo "\____/\___/|_| |_|___/\___/|_|\___\____/\/     "
-    echo "                                               "
-    echo "==============================================="
-    echo ""
-    echo "                 $MAIN_MENU"
-    echo ""
-    echo "==============================================="
-    echo ""
-    echo "              1. $MAIN_MENU1"
-    echo "              2. $MAIN_MENU2"
-    echo ""
-    echo "              3. $MAIN_MENU3"
-    echo "              4. $MAIN_MENU4"
-    echo "              5. $MAIN_MENU5"
-    echo "              6. $MAIN_MENU6"
-    echo ""
-    echo "              7. $MAIN_MENU7"
-    echo "              8. $MAIN_MENU8"
-    echo "              0. $LEXIT"
+    #echo "   ___                      _        ___   ___ " 
+    #echo "  / __\___  _ __  ___  ___ | | ___  / __\ / _ |" 
+    #echo " / /  / _ \| '_ \/ __|/ _ \| |/ _ \/ /   / /_)/"
+    #echo "/ /__| (_) | | | \__ \ (_) | |  __/ /___/ ___/ "
+    #echo "\____/\___/|_| |_|___/\___/|_|\___\____/\/     "
+    #echo "                                               "
+    #echo "==============================================="
+    #echo ""
+    #echo "                 $MAIN_MENU"
+    #echo ""
+    #echo "==============================================="
+    #echo ""
+    #echo "              1. $MAIN_MENU1"
+    #echo "              2. $MAIN_MENU2"
+    #echo ""
+    #echo "              3. $MAIN_MENU3"
+    #echo "              4. $MAIN_MENU4"
+    #echo "              5. $MAIN_MENU5"
+    #echo "              6. $MAIN_MENU6"
+    #echo ""
+    #echo "              7. $MAIN_MENU7"
+    #echo "              8. $MAIN_MENU8"
+    #echo "              0. $LEXIT"
+
+    center_text "   ___                      _        ___   ___ " $width
+    center_text "  / __\___  _ __  ___  ___ | | ___  / __\ / _ |" $width
+    center_text " / /  / _ \| '_ \/ __|/ _ \| |/ _ \/ /   / /_)/" $width
+    center_text "/ /__| (_) | | | \__ \ (_) | |  __/ /___/ ___/ " $width
+    center_text "\____/\___/|_| |_|___/\___/|_|\___\____/\/     " $width
+    echo "" 
+    center_text "===============================================" $width
+    echo "" 
+    center_text "                 $MAIN_MENU" $width
+    echo "" 
+    center_text "===============================================" $width
+    echo "" 
+    center_text "              1. $MAIN_MENU1" $width
+    center_text "              2. $MAIN_MENU2" $width
+    echo "" 
+    center_text "              3. $MAIN_MENU3" $width
+    center_text "              4. $MAIN_MENU4" $width
+    center_text "              5. $MAIN_MENU5" $width
+    center_text "              6. $MAIN_MENU6" $width
+    echo "" 
+    center_text "              7. $MAIN_MENU7" $width
+    center_text "              8. $MAIN_MENU8" $width
+    center_text "              0. $LEXIT" $width
+
 
     tput cup $(tput lines) 0
     read -p "$ENTER_NUMBER" choice
@@ -1012,6 +1046,7 @@ load_language_resources
             /tmp/update.sh
             ;;
         0)
+            clear
             exit 0
             ;;
         *)
